7ea80fa3f2c4b010b6f75dee2a88cb39e1e6b13e
Dmitry Tebryaev <tebr5923@mail.ru>
2023-02-19 19:57:44 +0700

    add user.js

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,21 +1,33 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.kata.spring.boot_security.demo.model.User;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping()
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/user")
     public String showMe(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("user", user);
         return "users/show";
+    }
+
+    @ResponseBody
+    @GetMapping(path ="/api/v1/user",  produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<User> getUser(@AuthenticationPrincipal User user){
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
 }
---
+++ src/main/resources/static/user.js
@@ -0,0 +1,36 @@
+$(async function () {
+    await userU();
+});
+
+async function userU() {
+    let url = "http://localhost:8080/api/v1/user"
+    let response = await fetch(url);
+    let {id, firstName, lastName, age, email, roles} = await response.json()
+
+    const aEmail = document.getElementById("emailId")
+    aEmail.textContent = email
+
+    const aRoles = document.getElementById("rolesId")
+    let textRoles = ""
+    roles.forEach(role => {
+        let roleLongName = role.name
+        let roleShortName = roleLongName.substring(5) + " "
+        textRoles += roleShortName
+    })
+    aRoles.textContent = `${textRoles}`
+
+    const userTableBody = document.getElementById("userTableBodyId")
+    const tableBody = `
+    <tr>
+                <td>${id}</td>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${age}</td>
+                <td>${email}</td>
+                <td>${textRoles}</td>            
+    </tr>
+    `
+    userTableBody.innerHTML = tableBody
+    // $(`#userTableBodyId`).append(tableBody);
+
+}
