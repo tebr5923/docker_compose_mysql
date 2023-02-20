a58161addf0e48e0785fd01ed4af02b279f8fce3
Dmitry Tebryaev <tebr5923@mail.ru>
2023-02-20 21:46:56 +0700

    add list.js

---
+++ src/main/resources/static/list.js
@@ -0,0 +1,48 @@
+$(async function () {
+    await list();
+});
+
+async function list() {
+    let url = "http://localhost:8080/api/v1/users"
+    let response = await fetch(url)
+    let users = await response.json()
+    users.forEach(user => {
+        usersToTableBody(user)
+    })
+}
+
+function usersToTableBody({id, firstName, lastName, age, email, roles}) {
+    let textRoles = ""
+    roles.forEach(role => {
+        let roleLongName = role.name
+        let roleShortName = roleLongName.substring(5) + " "
+        textRoles += roleShortName
+    })
+
+    const userTableBody = document.getElementById("tBodyAllUsersId")
+    const tableBody = `
+    <tr>
+                <td>${id}</td>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${age}</td>
+                <td>${email}</td>
+                <td>${textRoles}</td>             
+                <td>
+                     <button type="button" class="btn btn-info editBtn" data-toggle="modal"
+                        data-target="#edit"
+                        onclick="editUserData(${id})">
+                        Edit
+                    </button>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-danger"
+                        data-toggle="modal"
+                        data-target="#delete"
+                        onclick="deleteUserData(${id})">
+                        Delete
+                    </button>
+                </td>
+    </tr>`
+    userTableBody.insertAdjacentHTML('beforeend', tableBody)
+}
