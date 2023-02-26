f7f63c3a2d170eb7ad9fce90249c5b3f5fd89ef7
Dmitry Tebryaev <tebr5923@mail.ru>
2023-02-26 17:31:39 +0700

    add new.js

--- src/main/resources/static/new.js
+++ src/main/resources/static/new.js
@@ -1,9 +1,53 @@
-// async function showAllRole() {
-//     let dbRoles = [];
-//     let roles = await fetch("http://localhost:8080/api/v1/roles");
-//     await roles.json().then(roles => {
-//         roles.forEach(role =>
-//             dbRoles.push(role))
-//     });
-//     return dbRoles;
-// }
+
+async function showRole() {
+
+    const inputRoles = document.getElementById('nRoles');
+    let dbRoles = await showAllRole();
+    inputRoles.innerHTML = `
+            <option value="${dbRoles[0].id}">${dbRoles[0].shortName}</option>
+            <option value="${dbRoles[1].id}">${dbRoles[1].shortName}</option>
+            `
+}
+
+
+document.getElementById('profile-tab').addEventListener('click', showRole)
+
+document.getElementById('addNewUser').addEventListener('click', createUser)
+
+async function createUser() {
+    const inputFirstName = document.getElementById('nFirstName');
+    const inputLastName = document.getElementById('nLastName');
+    const inputAge = document.getElementById('nAge');
+    const inputEmail = document.getElementById('nEmail');
+    const inputPassword = document.getElementById('nPassword');
+
+
+    const firstName = inputFirstName.value;
+    const lastName = inputLastName.value;
+    const age = inputAge.value;
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+    let listRoles = await roleArray(document.getElementById('nRoles'));
+
+
+    if (firstName && lastName && age && email && password && (listRoles.length !== 0)) {
+
+        let res = await fetch(`http://localhost:8080/api/v1/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({firstName, lastName, age, email, password, roles: listRoles})
+        });
+        const result = await res.json();
+        await list()
+        $('#nav-home-tab').click();
+    }
+
+    inputFirstName.value = ''
+    inputLastName.value = ''
+    inputAge.value = ''
+    inputEmail.value = ''
+    inputPassword.value = ''
+
+}
